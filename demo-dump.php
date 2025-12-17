aa02aeb50601c77a0c29dbdb587ba132b8a0bd3d
B.V <bv201285@gmail.com>
2025-12-18 00:27:51 +0300

    test

--- demo-dump.php
+++ demo-dump.php
@@ -5,7 +5,7 @@ require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php';
  * @var CMain $APPLICATION
  */
 
- $APPLICATION->setTitle('Demo Dump');
+$APPLICATION->setTitle('Demo Dump222');
 
  dump(['e' => '123', '44' => 4]);
 
