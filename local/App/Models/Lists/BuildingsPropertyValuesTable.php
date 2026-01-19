45344b7538aaaf34cc10cef7f7292cd939e63b24
B.V <bv201285@gmail.com>
2026-01-19 17:03:16 +0300

    ДЗ_4

---
+++ local/App/Models/Lists/BuildingsPropertyValuesTable.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Lists;
+
+use App\Models\AbstractIblockPropertyValuesTable;
+
+class BuildingsPropertyValuesTable extends AbstractIblockPropertyValuesTable
+{
+    const IBLOCK_API_CODE = 'Buildings';
+
+}
--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -6,9 +6,12 @@ if (file_exists(__DIR__ . '/../../vendor/autoload.php'))
     require_once(__DIR__ . '/../../vendor/autoload.php');
 }
 
-//App
+// App
 if (file_exists(__DIR__ . '/../App/autoload.php'))
 {
     require_once(__DIR__ . '/../App/autoload.php');
 }
+
+// Events
+require __DIR__ . '/events.php';
 
