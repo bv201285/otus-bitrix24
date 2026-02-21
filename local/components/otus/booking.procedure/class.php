4a5334e63abd1e842a92dbf8bf56a51747b1a052
B.V <bv201285@gmail.com>
2026-02-21 17:06:13 +0300

    ДЗ #7

--- local/components/otus/booking.procedure/class.php
+++ local/components/otus/booking.procedure/class.php
@@ -166,8 +166,7 @@ class BookingProcPopupComponent extends CBitrixComponent implements Controllerab
 
 
         // dateString у вас в формате d.m.Y H:i:s
-        $dt = DateTime::createFromFormat('d.m.Y H:i:s', $dateString);
-        dd($dt);
+        $dt = $dt = \DateTime::createFromFormat('d.m.Y H:i:s', $dateString);
         if (!$dt) {
             return false;
         }
