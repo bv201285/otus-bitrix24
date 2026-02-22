140f9535978f65061579733413ea8985f63048d8
B.V <bv201285@gmail.com>
2026-02-22 16:38:50 +0300

    Доработка поля табл части

--- local/components/otus/main.field.deal_doctors_procedures_table/class.php
+++ local/components/otus/main.field.deal_doctors_procedures_table/class.php
@@ -2,12 +2,69 @@
 if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 
 use App\UserTypes\DealDoctorsProceduresTableUfField;
+use Bitrix\Iblock\ElementTable;
+use Bitrix\Iblock\IblockTable;
 use Bitrix\Main\Component\BaseUfComponent;
+use Bitrix\Main\Engine\Contract\Controllerable;
+use Bitrix\Main\Loader;
 
-class DealDoctorsProceduresTableUfComponent extends BaseUfComponent
+class DealDoctorsProceduresTableUfComponent extends BaseUfComponent implements Controllerable
 {
     protected static function getUserTypeId(): string
     {
         return DealDoctorsProceduresTableUfField::USER_TYPE_ID;
+    }
+
+    public function configureActions(): array
+    {
+        return [
+            'getElements' => ['prefilters' => []],
+        ];
+    }
+
+    public function getElementsAction(string $iblockApi): array
+    {
+        $iblockApi = trim($iblockApi);
+
+        if ($iblockApi === '') {
+            $this->addError(new Error('Empty iblockApi'));
+            return [];
+        }
+
+        if (!Loader::includeModule('iblock')) {
+            $this->addError(new Error('Module iblock not available'));
+            return [];
+        }
+
+        $allowed = ['Doctors' => true, 'Procedure' => true];
+        if (!isset($allowed[$iblockApi])) {
+            $this->addError(new Error('Iblock not allowed'));
+            return [];
+        }
+
+        $iblockRow = IblockTable::getRow([
+            'select' => ['ID'],
+            'filter' => ['=API_CODE' => $iblockApi],
+        ]);
+        if (!$iblockRow) {
+            $this->addError(new Error('Iblock not found'));
+            return [];
+        }
+
+        $iblockId = (int)$iblockRow['ID'];
+
+        $items = [];
+        $res = ElementTable::getList([
+            'select' => ['ID','NAME'],
+            'filter' => ['=IBLOCK_ID' => $iblockId, '=ACTIVE' => 'Y'],
+            'order' => ['NAME' => 'ASC'],
+            'limit' => 1000,
+        ]);
+
+        while ($row = $res->fetch()) {
+            $items[] = ['id' => (int)$row['ID'], 'name' => (string)$row['NAME']];
+        }
+
+        return ['items' => $items];
     }
 }
--- local/components/otus/main.field.smart_dependent/.description.php
+++ local/components/otus/main.field.smart_dependent/.description.php
@@ -2,6 +2,6 @@
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 
 $arComponentDescription = [
-    "NAME" => "main.field.smart_dependent",
-    "DESCRIPTION" => "CRM UF FieldComponent for smart_dependent",
+    "NAME" => "Составное поле",
+    "DESCRIPTION" => "Составное поле",
 ];
