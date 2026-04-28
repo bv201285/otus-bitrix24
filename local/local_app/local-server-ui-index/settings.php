c22212ac411375787095cca893f202a8bbe47cb5
B.V <bv201285@gmail.com>
2026-04-28 16:39:23 +0300

    local app

--- local/local_app/local-server-ui-index/settings.php
+++ local/local_app/local-server-ui-index/settings.php
@@ -1,6 +1,6 @@
 <?php
-//define('C_REST_CLIENT_ID','local.5c8bb1b0891cf2.87252039');//Application ID
-//define('C_REST_CLIENT_SECRET','SakeVG5mbRdcQet45UUrt6q72AMTo7fkwXSO7Y5LYFYNCRsA6f');//Application key
+define('C_REST_CLIENT_ID','local.69f0b8666fa5b4.08223891');//Application ID
+define('C_REST_CLIENT_SECRET','J7H5TSqH8K5Xcop0XTw4uOk4ZkeISJPRcxX2MSi4jPsrXYfw1p');//Application key
 // or
 //define('C_REST_WEB_HOOK_URL','https://rest-api.bitrix24.com/rest/1/doutwqkjxgc3mgc1/');//url on creat Webhook
 
